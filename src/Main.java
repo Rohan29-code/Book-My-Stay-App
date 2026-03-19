1f67672d0c64b4bd31932c1db1ef3c251e425ba4
Rohan29-code <pattapagalurohansai@gmail.com>
2026-03-19 22:33:11 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("=====================================");
+        System.out.println("     Welcome to Book My Stay App     ");
+        System.out.println("=====================================");
+        System.out.println("Application Name: Hotel Booking System");
+        System.out.println("Version: v1.0");
+        System.out.println("=====================================");
+    }
+}
